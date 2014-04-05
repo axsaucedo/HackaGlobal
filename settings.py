786070207d4603603b66a01a8425c769f1b0103c
Alejandro Saucedo <axsauze@Saucedo.local>
2014-04-05 13:53:54 +0300

    adapted theem

--- settings.py
+++ settings.py
@@ -64,7 +64,8 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = BASE_DIR + '/templates/static/'
+print STATIC_ROOT
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -72,9 +73,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    BASE_DIR + '/hackaglobal/static',
 )
 
 # List of finder classes that know how to find static files in

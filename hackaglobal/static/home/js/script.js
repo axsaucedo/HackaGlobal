62cad32f89d3475ed9e3fb4a2f2febb270a47488
Alejandro <axsauze@gmail.com>
2014-04-08 22:27:29 +0100

    added themes

--- hackaglobal/static/home/js/script.js
+++ hackaglobal/static/home/js/script.js
@@ -125,12 +125,22 @@ function createMap(data) {
 
     if ($('#googleMap').length > 0) {
 
+        var rand = Math.random();
+
+        //Normal theme
+        var style;
+        //Dark theme
+        if (rand < .3) style = [{"featureType":"all","stylers":[{"visibility":"off"}]},{featureType: "administrative.country",elementType: "labels","stylers": [ {visibility: "on"} ]},{"featureType":"water","stylers":[{"visibility":"on"},{"lightness":-100}]}];
+        //White theme
+        else if(rand < .6) style = [{"featureType":"administrative","stylers":[{"visibility":"off"}]},{featureType: "administrative.country",elementType: "labels","stylers": [ {visibility: "on"} ]},{"featureType":"poi","stylers":[{"visibility":"simplified"}]},{"featureType":"road","stylers":[{"visibility":"simplified"}]},{"featureType":"water","stylers":[{"visibility":"simplified"}]},{"featureType":"transit","stylers":[{"visibility":"simplified"}]},{"featureType":"landscape","stylers":[{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"visibility":"off"}]},{"featureType":"road.local","stylers":[{"visibility":"on"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"road.arterial","stylers":[{"visibility":"off"}]},{"featureType":"water","stylers":[{"color":"#5f94ff"},{"lightness":26},{"gamma":5.86}]},{},{"featureType":"road.highway","stylers":[{"weight":0.6},{"saturation":-85},{"lightness":61}]},{"featureType":"road"},{},{"featureType":"landscape","stylers":[{"hue":"#0066ff"},{"saturation":74},{"lightness":100}]}];
+
         map = new google.maps.Map(document.getElementById('googleMap'), {
             zoom: 2,
-            center: new google.maps.LatLng(40.7562008, -73.9903784),
+            center: new google.maps.LatLng(40.7562008, 73.9903784),
             mapTypeId: google.maps.MapTypeId.ROADMAP,
             disableDefaultUI: true,
-            scrollwheel: false
+            scrollwheel: false,
+            styles: style
         });
 
     }
@@ -196,7 +206,6 @@ function populateMap() {
 
 
 }// end populateMap()
-
 
 // manipulate the URL changes
 window.onpopstate = function(event) {
